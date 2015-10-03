776ddae22d41e01f5c9f5d181422e31a49748904
neekies <neekies652@gmail.com>
2015-10-03 00:17:35 -0500

    Some shit

--- codeforgood/src/main/java/codeforgood/model/Personal.java
+++ codeforgood/src/main/java/codeforgood/model/Personal.java
@@ -3,100 +3,46 @@ package codeforgood.model;
 public class Personal {
 
     private int points = 0;
-    private int Test;
-    private int ACT;
-    private int SAT;
-    public double GPA;
-    public String Awards;
-    public String Scholarships;
-
+    private String healthyDiet;
+    private String movePlaces;
+    private String sportAcievements;
+   
     public int getAcademicPoints() {
         return this.points;
     }
 
-    public int getPoints() {
-        return this.points;
-    }
 
-    public void setPoints(int points) {
+    public void setAcademicPoints(int points) {
         this.points = points;
     }
 
-    public int getTest() {
-        return this.Test;
-    }
+	public String getHealthyDiet() {
+		return healthyDiet;
+	}
 
-    public void setTest(int test) {
-        this.Test = test;
-    }
+	public void setHealthyDiet(String healthyDiet) {
+		this.healthyDiet = healthyDiet;
+		points=points+10;
+	}
 
-    public int getACT() {
-        return this.ACT;
-    }
+	public String getMovePlaces() {
+		return movePlaces;
+	}
 
-    public void setACT(int aCT) {
-        this.ACT = aCT;
-    }
+	public void setMovePlaces(String movePlaces) {
+		this.movePlaces = movePlaces;
+		points=points+10;
+	}
 
-    public int getSAT() {
-        return this.SAT;
-    }
+	public String getSportAcievements() {
+		return sportAcievements;
+	}
 
-    public void setSAT(int sAT) {
-        this.SAT = sAT;
-    }
+	public void setSportAcievements(String sportAcievements) {
+		this.sportAcievements = sportAcievements;
+		points=points+10;
+	}
 
-    public double getGPA() {
-        return this.GPA;
-    }
-
-    public void setGPA(double gPA) {
-        this.GPA = gPA;
-    }
-
-    public String getAwards() {
-        return this.Awards;
-    }
-
-    public void setAwards(String awards) {
-        this.Awards = awards;
-    }
-
-    public String getScholarships() {
-        return this.Scholarships;
-    }
-
-    public void setScholarships(String scholarships) {
-        this.Scholarships = scholarships;
-    }
-
-    public int setAcademicPoints(int newPoints) {
-        this.points = newPoints;
-        return this.points;
-    }
-
-    public void Test() {
-        this.points = this.points + 10;
-    }
-
-    public void ACT() {
-        this.points = this.points + 10;
-    }
-
-    public void SAT() {
-        this.points = this.points + 10;
-    }
-
-    public void GPA() {
-        this.points = this.points + 10;
-    }
-
-    public void Awards() {
-        this.points = this.points + 100;
-    }
-
-    public void Scholarship() {
-        this.points = this.points + 100;
-    }
+   
 
 }
