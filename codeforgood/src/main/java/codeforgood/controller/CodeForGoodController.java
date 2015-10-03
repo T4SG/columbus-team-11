b2e1ca3b5d40e6cba8e01acabde87bba03b70da6
Alex Edgar <aedgar77@gmail.com>
2015-10-02 22:36:04 -0400

    adding basic example

--- codeforgood/src/main/java/codeforgood/controller/CodeForGoodController.java
+++ codeforgood/src/main/java/codeforgood/controller/CodeForGoodController.java
@@ -4,15 +4,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import codeforgood.model.Profile;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 public class CodeForGoodController {
     
-    @RequestMapping("/hello")
-    public String index(Profile profile) {
-        return "Code For Good: " + profile.getUserId();
+    @RequestMapping("/update")
+    public String updateForm(Map<String, Object> model) {
+        return "update";
+    }
+    
+    @RequestMapping("/postUpdate")
+    public String postUpdate(@RequestParam(value="message", required=false) String message, Map<String,String> model) {
+    	model.put("message", message);
+        return "updateSuccess";
     }
     
 }
